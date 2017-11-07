479f5411dbb115224c697d583a1d59731b20b0cf
Matt Shine <matt-shine@hotmail.com>
2017-11-07 22:27:56 +1000

    Server now caches API responses for 10 minutes

--- src/server.js
+++ src/server.js
@@ -8,15 +8,49 @@ const router = express.Router();
 
 const API_URL = 'https://api.icowatchlist.com/public/v1/';
 
-router.get('/icos/:filter', (req, res) => {
+const store = {};
 
+router.get('/icos/:filter', (req, res) => {
+  
   const { filter } = req.params;
-	axios.get(`${API_URL}/${filter}`).then((response) => {
-    console.log(`Got request for: ${filter}`)
-		res.send(response.data.ico[filter]);
-	});
-	
+  
+  console.log(`Got request for: ${filter}`)
+
+  let now = new Date();
+
+  if (store.lastRequest) {
+    let tenMinutes = 10 * 60000;
+    let tenMinuteSAgo = new Date(now - tenMinutes);
+    if (tenMinuteSAgo > store.lastRequest) {
+      //Store is stale - refetch
+      fetchAll().then(() => {
+        res.send(store[filter])
+      })
+    } else {
+      //No need to refetch
+      res.send(store[filter]);
+    }
+  } else {
+    //Initial population of store
+    fetchAll().then(() => {
+      res.send(store[filter])
+    })
+  }
+
 });
+
+function fetchAll() {
+  return new Promise((resolve,reject) => {
+    axios.get(`${API_URL}`).then((response) => {
+        const { live, upcoming, finished } = response.data.ico;
+        store.live = live; 
+        store.upcoming = upcoming;
+        store.finished = finished;
+        store.lastRequest = new Date()
+        resolve();
+      });
+  })
+}
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
